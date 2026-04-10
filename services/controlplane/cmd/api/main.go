70d9f2391dcf23adcf5ab726d8a52dac93126b2a
fangyan33 <fangyan33@gmail.com>
2026-04-10 02:41:52 +0000

    让 controlplane 入口常驻运行

--- services/controlplane/cmd/api/main.go
+++ services/controlplane/cmd/api/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("controlplane api starting")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("ok\n"))
+	})
+
+	addr := ":8080"
+	log.Printf("controlplane api listening on %s", addr)
+	if err := http.ListenAndServe(addr, mux); err != nil {
+		log.Fatal(err)
+	}
 }
